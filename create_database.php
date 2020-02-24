6b766c62970decb4b03012fd7b5eda50ebd81ef3
Jim Garrett <jimgarrett123@gmail.com>
2020-02-24 14:22:52 -0500

    Update create_database.php

--- create_database.php
+++ create_database.php
@@ -1,7 +1,7 @@
 <?php
 // Define configfile path
 $db_config_file = "/data/db.php";
-//
+// hello
 if (file_exists($db_config_file)) {
 	// include config file if we have found it
 	require_once($db_config_file);
